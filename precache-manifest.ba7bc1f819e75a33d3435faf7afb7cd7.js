d47cf65d69fb8c1ce0ba2d3f65bf08b3fc54f1b1
Ridwan <kadriridwan0@gmail.com>
2019-11-13 22:46:34 +0000

    Updates

---
+++ precache-manifest.ba7bc1f819e75a33d3435faf7afb7cd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc19abdd394dbaa89a5511ca35eddb9",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "04130c833c2154f9d603",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fbb405d19a1f80982c29",
+    "url": "/robofriends/static/css/main.dde99b16.chunk.css"
+  },
+  {
+    "revision": "04130c833c2154f9d603",
+    "url": "/robofriends/static/js/2.ada142a8.chunk.js"
+  },
+  {
+    "revision": "fbb405d19a1f80982c29",
+    "url": "/robofriends/static/js/main.cb1b67db.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
